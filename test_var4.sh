221103a590525c3d99bfd85de512c1318362c266
AngryReiser <Halk001000@mail.ru>
2017-09-16 14:27:59 +0300

    Update test_var4.sh

--- test_var4.sh
+++ test_var4.sh
@@ -4,8 +4,8 @@
 testEquality()
 {
 	sh 5.sh
-	actual=`cat res_4.xml`
-	expected=`cat result.xml`
+	actual=`cat actual_4.txt`
+	expected=`cat expected_4.txt`
   assertEquals "$actual" "$expected"
 }
 
