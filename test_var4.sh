88047237d20648247d36ff35cf126cb1ba61da12
AngryReiser <Halk001000@mail.ru>
2017-09-16 14:23:01 +0300

    Create test_var4.sh

---
+++ test_var4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# file: examples/equality_test.sh
+
+testEquality()
+{
+	sh 5.sh
+	actual=`cat res_4.xml`
+	expected=`cat result.xml`
+  assertEquals "$actual" "$expected"
+}
+
+# load shunit2
+. shunit2-2.1.6/src/shunit2
