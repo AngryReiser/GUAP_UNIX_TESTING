d145f07762129df1e36a4181a93343088b864113
AngryReiser <Halk001000@mail.ru>
2017-09-24 01:09:08 +0300

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -2,7 +2,7 @@
 
 d1=`date +"%d-%m-%Y %T" -d @$(cat dns-tunneling.log | awk ' {print $4} ' | sort -nk 1 |awk -F'.' '{print $1}' | head -n 1)`;
 ns1=`cat dns-tunneling.log | awk ' {print $4} ' | sort -nk 1 | awk -F'.' '{print $2}' | head -n 1 | head -c 2`; 
-echo "First querry: $d1.$ns1">>actual_4.txt;
+echo "First querry: $d1.$ns1">actual_4.txt;
 
 d2=`date +"%d-%m-%Y %T" -d @$(cat dns-tunneling.log | awk ' {print $4} ' | sort -nk 1 | awk -F'.' '{print $1}' | tail -n 1)`; 
 ns2=`cat dns-tunneling.log | awk ' {print $4} ' | sort -nk 1 | awk -F'.' '{print $2}' | head -n 1 | head -c 2`;
